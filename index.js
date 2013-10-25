95fe030a626ecaa95e6b14b99686313a0f1d3a23
Cameron Bytheway <cameron@theflokk.com>
2013-10-25 15:22:26 -0600

    encode/decode overrides in smaller format

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ var emitter = new Emitter;
  */
 
 exports = module.exports = function(feature, ignoreCookie) {
-  var enabled = get()[feature];
+  var features = get();
+  var enabled = features[feature];
   if (typeof enabled !== 'undefined' || ignoreCookie) return !!enabled;
   return cookieValue(feature);
 };
@@ -39,7 +40,7 @@ exports = module.exports = function(feature, ignoreCookie) {
 
 exports.enable = function(feature) {
   var features = get();
-  features[feature] = 1;
+  features[feature] = true;
   set(features);
   emitter.emit(feature, true);
   return exports;
@@ -55,7 +56,7 @@ exports.enable = function(feature) {
 
 exports.disable = function(feature) {
   var features = get();
-  features[feature] = 0;
+  features[feature] = false;
   set(features);
   emitter.emit(feature, false);
   return exports;
@@ -116,15 +117,25 @@ exports.watch = function(feature, fn) {
 exports.KEY = 'features';
 
 /**
+ * Keep a cached version of the enabled features
+ */
+
+var cache;
+
+/**
  * Get the localstorage content
  *
  * @api private
  */
 
 function get() {
-  if (!store.supported) return {};
-  return store(exports.KEY) || {};
-};
+  if (cache) return cache;
+  var value = store.supported
+    ? store(exports.KEY)
+    : cookie('_' + exports.KEY);
+  cache = decode(value);
+  return cache;
+}
 
 /**
  * Set the localstorage content
@@ -133,9 +144,41 @@ function get() {
  */
 
 function set(value) {
-  if (!store.supported) return;
-  store(exports.KEY, value);
-};
+  var encoded = encode(value);
+  store.supported
+    ? store(exports.KEY, encoded)
+    : cookie('_' + exports.KEY, encoded);
+  cache = value;
+}
+
+/**
+ * Encode enabled values in small format
+ */
+
+function encode(obj) {
+  if (!obj) return obj;
+  var str = '';
+  each(obj, function(key, value) {
+    str = str + (value ? '*' : '!') + key;
+  });
+  return str;
+}
+
+/**
+ * Decode enabled values from a small format
+ */
+
+function decode(str) {
+  var obj = {};
+  if (!str || !str.split) return obj;
+  var list = str.split(/([!\*])/);
+  // Remove the first space if it's ""
+  if (list[0] === '') list.shift();
+  for (var i = 0; i < list.length; i += 2) {
+    obj[list[i + 1]] = list[i] === '*';
+  };
+  return obj;
+}
 
 /**
  * Get the cookie value
@@ -144,4 +187,4 @@ function set(value) {
 function cookieValue(feature) {
   var features = (cookie(exports.KEY) || '').split(',');
   return !!~index(features, feature);
-};
+}
